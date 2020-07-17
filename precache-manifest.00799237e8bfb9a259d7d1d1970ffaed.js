2f529b9ad9f78cd94065b942a9765d6280bfffcb
Paeddel <paddystaf@gmail.com>
2020-07-17 11:45:43 +0200

    Updates

---
+++ precache-manifest.00799237e8bfb9a259d7d1d1970ffaed.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1dec7db00338ea979339b3fad093d4c0",
+    "url": "/Paeddel/GartenApp/index.html"
+  },
+  {
+    "revision": "717166acb7f5139f8457",
+    "url": "/Paeddel/GartenApp/static/css/2.89f067aa.chunk.css"
+  },
+  {
+    "revision": "0bae9aff695338588068",
+    "url": "/Paeddel/GartenApp/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "717166acb7f5139f8457",
+    "url": "/Paeddel/GartenApp/static/js/2.5ae3bdd0.chunk.js"
+  },
+  {
+    "revision": "85b12af60ddf57ea6860c3b16407fd52",
+    "url": "/Paeddel/GartenApp/static/js/2.5ae3bdd0.chunk.js.LICENSE"
+  },
+  {
+    "revision": "0bae9aff695338588068",
+    "url": "/Paeddel/GartenApp/static/js/main.4031cea7.chunk.js"
+  },
+  {
+    "revision": "665cb0777dacbe884fe3",
+    "url": "/Paeddel/GartenApp/static/js/runtime-main.6b52b70c.js"
+  }
+]);
